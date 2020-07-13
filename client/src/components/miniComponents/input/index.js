9e6d71b30f81f3031f4beceb7dbd4bce8ed9c7c2
mabutalid1 <marcobutalid989@gmail.com>
2020-07-13 15:11:56 +0800

    fixed footer/added Containers/ newPage Controls

--- client/src/components/miniComponents/input/index.js
+++ client/src/components/miniComponents/input/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Input({placeholder}){
     return(
         <div>
-            <input class="w-full mt-3 rounded-sm border border-gray-400 focus:outline-none focus:border-blue-500" placeholder={placeholder}/>
+            <input className="w-full mt-3 rounded-sm border border-gray-400 focus:outline-none focus:border-blue-500" placeholder={placeholder}/>
         </div>
     )
 }
