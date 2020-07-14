085338bdae065d7c1036303bc180caa578e479ff
mabutalid1 <marcobutalid989@gmail.com>
2020-07-14 21:20:31 +0800

    asd

--- util/database.js
+++ util/database.js
@@ -3,12 +3,7 @@ const Sequelize = require('sequelize');
 
 //dotenv
 require('dotenv').config();
-let dbName = process.env.DB_NAME;
-let dbHost = process.env.DB_HOST;
-let dbUser = process.env.DB_USER;
-let dbPass = process.env.DB_PASS;
-
-sequelize = new Sequelize(process.env.DB_URL, {
+sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
    }),
 
