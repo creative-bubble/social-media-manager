f09e8f75fc3c158efc907807c27b9f3c0ecf8c5f
mabutalid1 <marcobutalid989@gmail.com>
2020-07-15 14:06:56 +0800

    change env variables to heroku db env variables

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -2,15 +2,8 @@ const Sequelize = require('sequelize');
 
 //dotenv
 require('dotenv').config();
-let dbName = process.env.DB_NAME;
-let dbHost = process.env.DB_HOST;
-let dbUser = process.env.DB_USER;
-let dbPass = process.env.DB_PASS;
-
-
-const sequelize = new Sequelize(dbName, dbUser, dbPass, {
- dialect: 'mysql',
- host: dbHost
-});
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres'
+   }),
 
 module.exports = sequelize;
