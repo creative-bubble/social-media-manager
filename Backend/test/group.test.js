73ff0acef64121a7ee551d8f199092c596ba20f9
mabutalid1 <marcobutalid989@gmail.com>
2020-07-25 21:46:36 +0800

    unit test for post req on group API

--- Backend/test/group.test.js
+++ Backend/test/group.test.js
@@ -1,1 +1,21 @@
+const request = require('supertest');
 const app = require('../app');
+const Group = require('../models/group');
+
+beforeAll(async () => {
+    await Group.destroy({
+        where: {}
+    })
+})
+
+it('Should create group expecting status code 201', async (done) => {
+    await request(app).post('/group')
+    .send({
+        name: "GroupTestExample"
+    })
+    .expect(201);
+    done();
+})
+
+
+
