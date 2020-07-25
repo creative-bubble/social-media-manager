dbd8cac816a05f9cac4558e4b56688d08cb8f15f
mabutalid1 <marcobutalid989@gmail.com>
2020-07-25 21:32:20 +0800

    separated middleware/routes from server listen

---
+++ Backend/app.js
@@ -0,0 +1,57 @@
+//npm packages
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+//routes
+const homeRoutes = require('./routes/home');
+const groupRoutes = require('./routes/group');
+const roleRoutes = require('./routes/roles');
+//swagger documentation
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+//variables
+const app = express();
+require('dotenv').config();
+let port = process.env.PORT || 5000;
+
+//swagger configuration
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'API',
+            description: 'API DOCUMENTATION',
+            contact: {
+                name: "Marco Butalid"
+            },
+            servers: [`http://localhost:${port}`]
+        }
+    },
+    apis: ['./routes/*.js']
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
+
+
+
+//middleware setups
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+//middleware routes
+app.use(homeRoutes);
+app.use(groupRoutes);
+app.use(roleRoutes);
+
+
+module.exports = app;
