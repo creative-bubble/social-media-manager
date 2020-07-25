a67e63e51619125f143faa0c41e1083974e294b4
mabutalid1 <marcobutalid989@gmail.com>
2020-07-25 21:25:38 +0800

    added dev.env & test.env

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -2,8 +2,15 @@ const Sequelize = require('sequelize');
 
 //dotenv
 require('dotenv').config();
-sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres'
-   }),
+// sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: 'postgres'
+//    }),
+
+sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    dialect: process.env.DB_DIALECT || 'postgres',
+    host: process.env.DB_HOST
+}),
+
+
 
 module.exports = sequelize;
