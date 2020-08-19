f0293d58795a7cfb3bfc3b8a45a24ef7eb5d066e
mabutalid1 <marcobutalid989@gmail.com>
2020-08-19 13:26:29 +0800

    association adde

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 //local modules
 const app = require('./app');
+const associations = require('./util/association');
 require('dotenv').config();
 let port = process.env.PORT || 9000;
 
+associations();
 
 app.listen(port, () => {
     console.log(`Server listening on ${port}`)
