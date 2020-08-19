2fff402b493d74d0adca0e35980be43ecef887ac
mabutalid1 <marcobutalid989@gmail.com>
2020-08-19 11:36:32 +0800

    empty the master so that no conflict dring merge

--- index.js
+++
@@ -1,9 +0,0 @@
-//local modules
-const app = require('./app');
-require('dotenv').config();
-let port = process.env.PORT || 9000;
-
-
-app.listen(port, () => {
-    console.log(`Server listening on ${port}`)
-});
